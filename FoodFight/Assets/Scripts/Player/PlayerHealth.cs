63b587da74248cf62e77bdf9610fc01002c82827
Ben Hoffman <bah8892@g.rit.edu>
2017-04-09 15:15:40 -0400

    Fixed up weapon fireing

--- FoodFight/Assets/Scripts/Player/PlayerHealth.cs
+++ FoodFight/Assets/Scripts/Player/PlayerHealth.cs
@@ -2,39 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+
 // Caleb Katzenstein
 // Handles player health
 public class PlayerHealth : MonoBehaviour 
 {
 	// the health when first starting
-	int startingHealth;
+	public float startingHealth;
 	// stores the health at any given moment
-	int currentHealth;
-	// Use this for initialization
+	private float currentHealth;
+
+	/// <summary>
+    /// Set the current health to the start health
+    /// </summary>
 	void Start () 
 	{
-		startingHealth = 100;
 		currentHealth = startingHealth;
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
 	// if player collides with a meatball, this loses health
-	public void OnCollisionEnter2D(Collision2D other)
+	private void OnCollisionEnter2D(Collision2D other)
 	{
+        // If we collide with a meatball...
 		if (other.gameObject.CompareTag ("Meatball")) 
 		{
-			currentHealth -= 20;
+            // Take damage based on the mass of the meatball
+			currentHealth -= other.gameObject.GetComponent<Rigidbody2D>().mass;
+
 			// prints current health
-			print (currentHealth);
+			print (currentHealth);          
+
+            // If the player has a health of less than 0...
 			if (currentHealth <= 0) 
 			{
-				// restarts the program
-				EditorSceneManager.LoadSceneAsync (EditorSceneManager.GetActiveScene().buildIndex);
+                // Handle player death
+                Die();
 			}
 		}
 	}
+
+    /// <summary>
+    /// Handles player death
+    /// </summary>
+    private void Die()
+    {
+        Debug.Log(this.name + " is dead!");
+
+        // Tell the game manager that this player has died
+    }
 }
--- FoodFight/Assets/Scripts/Player/PlayerWeaponController.cs
+++ FoodFight/Assets/Scripts/Player/PlayerWeaponController.cs
@@ -18,6 +18,9 @@ public class PlayerWeaponController : MonoBehaviour
     private string fire_input_string = "Fire1";     // The name of the input button for firing
     private float _timeSinceLastShot;               // The time since we last shot
 
+    private Transform rotateAround;
+    private Vector3 v;
+
     #endregion
 
     /// <summary>
@@ -28,6 +31,11 @@ public class PlayerWeaponController : MonoBehaviour
     {
         // Set our time since last fire to the start fire rate
         _timeSinceLastShot = fireRate;
+
+        rotateAround = GetComponentInParent<Transform>();
+
+        v = (meatballSpawnZone.position - rotateAround.position);
+
     }
 
     /// <summary>
@@ -35,11 +43,16 @@ public class PlayerWeaponController : MonoBehaviour
     /// </summary>
 	void Update ()
     {
+        // Handle aiming of the weapon
+        UpdateWeaponRotation();
+
         // If the player wants to shoot, and we can...
-		if(Input.GetButtonDown(fire_input_string) && _timeSinceLastShot <= fireRate)
+		if(Input.GetKeyDown(KeyCode.Q) && _timeSinceLastShot >= fireRate)
         {
             // Fire a shot
             Fire();
+            // Reset the time since last shot
+            _timeSinceLastShot = 0f;
         }
         // We are not allowed to shoot...
         else
@@ -47,9 +60,41 @@ public class PlayerWeaponController : MonoBehaviour
             // Add to our time since the last shot, since we haven't shot
             _timeSinceLastShot += Time.deltaTime;
         }
-
 	}
 
+    /// <summary>
+    /// This will allow the user to aim around their gun using the right anaolog
+    /// or the mouse movement
+    /// </summary>
+    private void UpdateWeaponRotation()
+    {
+        // Get the screen point of the center of the screeen
+        Vector3 centerScreenPos = Camera.main.WorldToScreenPoint(rotateAround.position).normalized;
+
+        // Get player inpput from the joysticks
+        float yRot = Input.GetAxis("Mouse Y") * 60f;
+        float xRot = Input.GetAxis("Mouse X") * 45f;
+
+        // Create an input vector based on the playe rinput that we have
+        Vector3 INPUT = new Vector3(yRot, xRot, 0F);
+
+        // If we are not giving input then ditch this
+        if (INPUT.magnitude <= 0f)
+        {
+            return;
+        }
+        // Our directino is our input minus the center of the screen poition
+        Vector3 dir = INPUT - centerScreenPos;
+
+        // Calculate the angle that we should be facing
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+        // Create a quaterionon based on that angle
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Set our position 
+        meatballSpawnZone.position = rotateAround.position + q * v;
+        // Set our rotiation
+        meatballSpawnZone.rotation = q;
+    }
 
     /// <summary>
     /// Actually fire a shot in the directoin that the player is aiming
