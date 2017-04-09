cf6f265711802bed9638c8734605b4855b36e0eb
Ben Hoffman <bah8892@g.rit.edu>
2017-04-08 22:55:33 -0400

    Created a menu scene, started working on game manager

---
+++ FoodFight/Assets/Scripts/PlayerOption.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This is an enum that will hold the player option, for the player select screen
+/// </summary>
+public enum PlayerOption
+{
+    Rigatoni,
+    Raviolli,
+    AngelHair,
+    Penne,
+    Linguine
+}
