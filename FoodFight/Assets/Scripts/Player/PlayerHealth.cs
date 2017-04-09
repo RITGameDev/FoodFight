01de955cc757cc5bc5f61bf16cdc81cb676e84f0
boringlawyer <cnk5777@rit.edu>
2017-04-09 00:43:36 -0400

    Added a PlayerHealth class.
    Added a PlayerHealth class. Added a tag to the meatball prefab to make
    it identifiable. Currently it prints the current health of the player to
    the console. To get rid of that, just go to
    PlayerHealth.OnCollisionEnter2D to get rid of the print method.

---
+++ FoodFight/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+// Caleb Katzenstein
+// Handles player health
+public class PlayerHealth : MonoBehaviour 
+{
+	// the health when first starting
+	int startingHealth;
+	// stores the health at any given moment
+	int currentHealth;
+	// Use this for initialization
+	void Start () 
+	{
+		startingHealth = 100;
+		currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	// if player collides with a meatball, this loses health
+	public void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag ("Meatball")) 
+		{
+			currentHealth -= 20;
+			// prints current health
+			print (currentHealth);
+			if (currentHealth <= 0) 
+			{
+				// restarts the program
+				EditorSceneManager.LoadSceneAsync (EditorSceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+}
