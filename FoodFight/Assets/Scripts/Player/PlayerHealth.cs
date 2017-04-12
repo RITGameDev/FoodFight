7f8ed40b4f03174bc21462b14f261fc12ca8105b
Ben Hoffman <bah8892@g.rit.edu>
2017-04-11 22:20:48 -0400

    For some reason shooting was wrong, so I fixed that

--- FoodFight/Assets/Scripts/Player/PlayerHealth.cs
+++ FoodFight/Assets/Scripts/Player/PlayerHealth.cs
@@ -5,41 +5,41 @@ using UnityEditor.SceneManagement;
 
 // Caleb Katzenstein
 // Handles player health
-public class PlayerHealth : MonoBehaviour 
+public class PlayerHealth : MonoBehaviour
 {
-	// the health when first starting
-	public float startingHealth;
-	// stores the health at any given moment
-	private float currentHealth;
+    // the health when first starting
+    public float startingHealth;
+    // stores the health at any given moment
+    private float currentHealth;
 
-	/// <summary>
+    /// <summary>
     /// Set the current health to the start health
     /// </summary>
-	void Start () 
-	{
-		currentHealth = startingHealth;
-	}
-	
-	// if player collides with a meatball, this loses health
-	private void OnCollisionEnter2D(Collision2D other)
-	{
-        // If we collide with a meatball...
-		if (other.gameObject.CompareTag ("Meatball")) 
-		{
-            // Take damage based on the mass of the meatball
-			currentHealth -= other.gameObject.GetComponent<Rigidbody2D>().mass;
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
 
-			// prints current health
-			print (currentHealth);          
+    // if player collides with a meatball, this loses health
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // If we collide with a meatball...
+        if (other.gameObject.CompareTag("Meatball"))
+        {
+            // Take damage based on the mass of the meatball
+            currentHealth -= other.gameObject.GetComponent<Rigidbody2D>().mass;
+
+            // prints current health
+            print(currentHealth);
 
             // If the player has a health of less than 0...
-			if (currentHealth <= 0) 
-			{
+            if (currentHealth <= 0)
+            {
                 // Handle player death
                 Die();
-			}
-		}
-	}
+            }
+        }
+    }
 
     /// <summary>
     /// Handles player death
