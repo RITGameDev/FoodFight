66add4b84c5ef2328f27cda61d261074202c51c3
Ben Hoffman <bah8892@g.rit.edu>
2017-04-11 23:56:48 -0400

    Set up the UI a lot more, mmm yea buttons

--- FoodFight/Assets/Scripts/Menu/PlayerSelection/PlayerPanel.cs
+++ FoodFight/Assets/Scripts/Menu/PlayerSelection/PlayerPanel.cs
@@ -9,22 +9,95 @@ using UnityEngine;
 /// player number for each panel, we need to bind their controller inpt to 
 /// that number character. 
 /// 
-/// 
 /// Author: Ben Hoffman
 /// </summary>
 public class PlayerPanel : MonoBehaviour {
 
     public PlayerOption selectedCharacter;  // Which character we have selected.
+    public UnityEngine.UI.Image placeHolderImage;   // The place holder image for the UI
+    public UnityEngine.UI.Scrollbar scrollBar;
 
     private bool isUsed;                    // Shows whether or not this panel is being used or not
-
-
-    public bool IsUsed { get; set; }
+    private PlayerOptionContainer[] options;
+    private int currentSelectionIndex;      // The option that we currently have selected
+    private int prevIndex;
+    public bool IsUsed { get { return isUsed; } set { isUsed = value; } }
 	
-
-	public void SetCurrentPlayerOption()
+    private void Start()
     {
+        // Set the placeholder image as active
+        placeHolderImage.gameObject.SetActive(true);
 
+        // Get all the options that are available 
+        options = GetComponentsInChildren<PlayerOptionContainer>();
+
+        // Set the number of steps in the scroll bar to how many options we have
+        scrollBar.numberOfSteps = options.Length;
+
+        scrollBar.value = 1;
+    }
+
+    /// <summary>
+    /// Use this with the buttons for the selection menu, 
+    /// 
+    /// Author: Ben Hoffman
+    /// </summary>
+    /// <param name="direction">Move up or down in my selcetion. True is DOWN, false is UP</param>
+    public void ChangeOption(bool direction)
+    {
+        // Keep track of the previous index so we can change it's color back
+        prevIndex = currentSelectionIndex;
+
+        // Move my index 1 in the direction that we want to 
+        if (direction)
+        {
+            currentSelectionIndex++;
+        }
+        else
+        {
+            currentSelectionIndex--;
+        }
+        
+        // Make sure that we are in the right range of values
+        if(currentSelectionIndex >= options.Length)
+        {
+            currentSelectionIndex = options.Length - 1;
+            // Return out of this method so that we don't move the scroll bar anymore
+        }
+        else if(currentSelectionIndex < 0)
+        {
+            currentSelectionIndex = 0;
+            // Return out of this method so that we don't move the scroll bar anymore
+        }
+
+        // Move the lsier value
+        MoveSlider();
+    }
+
+    private void MoveSlider()
+    {
+        // Change the value of the slider
+        scrollBar.value = (options.Length - currentSelectionIndex) * (1f / scrollBar.numberOfSteps);
+        if (currentSelectionIndex != prevIndex)
+        {
+
+            // Highlight the currently selected index
+            options[currentSelectionIndex].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+            // Un-highlight the previous index
+            options[prevIndex].GetComponent<UnityEngine.UI.Image>().color = Color.white;
+        }
+
+    }
+
+    /// <summary>
+    /// This will be used to select the current option 
+    /// that the user is on
+    /// </summary>
+    public void SelectCurrrentOption()
+    {
+        Debug.Log("Current option:  " + options[currentSelectionIndex].character.ToString()); 
+
+        // Play some kind of particle effect or something on that selection
     }
 
     /// <summary>
@@ -34,7 +107,33 @@ public class PlayerPanel : MonoBehaviour {
     {
         // We are using this panel now
         isUsed = true;
-        
+
         // Show the player options that we have, instead of 'Press A To start'
+        placeHolderImage.gameObject.SetActive(false);
+
+        // Set our current selection to the first one
+        currentSelectionIndex = 0;
+
+        prevIndex = currentSelectionIndex;
+
+        // Set the selection button as active
+        MoveSlider();
+
+        options[currentSelectionIndex].GetComponent<UnityEngine.UI.Image>().color = Color.blue;
+
+    }
+
+    /// <summary>
+    /// Put the placeholder text back in front of everything,
+    /// mark this object as un-used
+    /// 
+    ///  Author: Ben Hoffman
+    /// </summary>
+    public void Reset()
+    {
+        // We are not being used anymore
+        isUsed = false;
+        // Put the place holder image back up
+        placeHolderImage.gameObject.SetActive(true);
     }
 }
--- FoodFight/Assets/Scripts/Menu/PlayerSelection/PlayerSelectScreen.cs
+++ FoodFight/Assets/Scripts/Menu/PlayerSelection/PlayerSelectScreen.cs
@@ -6,29 +6,26 @@ using UnityEngine;
 /// This will hold an array of player pannel objects that are all
 /// 'inactive' to start.
 /// 
-/// 
 /// Author: Ben Hoffman
 /// </summary>
 public class PlayerSelectScreen : MonoBehaviour {
 
-    private PlayerPanel[] panels;
+    public PlayerPanel[] panels;       // The panels tha we have for the players to choose from
 
-	// Use this for initialization
+	/// <summary>
+    /// Get the panel components on this game object
+    /// </summary>
 	void Start ()
     {
         // Get the player panel componenets
         panels = GetComponentsInChildren<PlayerPanel>();
-
-        // Deactive all of them
-	    for(int i = 0; i < panels.Length; i++)
-        {
-            // Set each one as inactiev
-        }	
 	}
 	
 	/// <summary>
     /// Listen for player input and determine if we need to mark a panel
     /// as active or not
+    /// 
+    /// Author: Ben Hoffman
     /// </summary>
 	void Update () 
     {
@@ -49,13 +46,15 @@ public class PlayerSelectScreen : MonoBehaviour {
             }
 
             // There are no more player objects to active
-            
+            Debug.Log("There are no more player spots available!");
         }
     }
 
     /// <summary>
     ///  This will give all the info we have on the player options to the game
     ///  controller, so that we can instantiate all of them
+    ///  
+    /// Author: Ben Hoffman
     /// </summary>
     public void StartGame()
     {
